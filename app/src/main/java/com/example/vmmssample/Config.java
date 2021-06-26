afcf526f11c841b8b682a42ef3f094c1bd4f01d5
Naga Chandrika Yalla <s160516@rguktsklm.ac.in>
2021-06-26 09:43:29 +0530

    initial commit

---
+++ app/src/main/java/com/example/vmmssample/Config.java
@@ -0,0 +1,7 @@
+package com.example.vmmssample;
+
+public class Config {
+    public static String EMAIL ="chandrikayalla.516@gmail.com";  //gmail address
+    public static String PASSWORD ="chandrika 456"; //password
+}
+
